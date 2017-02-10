22584c91786d38ff7bba88e9c308bcae3fd28782
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2017-02-09 23:29:09 -0600

    does a bunch of stuff on part1 of assignment, working on sorting eigenvals

--- eigendigits/eigendigits.py
+++ eigendigits/eigendigits.py
@@ -13,13 +13,11 @@ vector of A,
 eigenvectors of the covariance matrix of A (after the mean has been subtracted). 
 -Columns of V are sorted in descending order by eigenvalue
 '''
-def sample_images(testImages, num_samples):
-	print np.shape(testImages)
-	indices = np.random.randint(np.shape(testImages)[2], size=num_samples)
-	print indices
+def sample_images(images, num_samples):
+	indices = np.random.randint(np.shape(images)[2], size=num_samples)
 	sample = np.zeros((28, 28, num_samples))
 	for i in range(len(indices)):
-		sample[:,:,i] = testImages[:,:,indices[i]]
+		sample[:,:,i] = images[:,:,indices[i]]
 	#print sample
 	#print "that was sample"
 	return sample
@@ -28,8 +26,22 @@ def vectorize_images(images):
 	return np.reshape(images, (28 * 28, np.shape(images)[2]))
 
 def hw1FindEigendigits(matrix):
-	print "nothing"
-	#vectorize_images()
+	new_mat = mean_subtract(matrix)
+	eigen_info = eigenvalues(a_transpose_a(new_mat))
+	eigenvectors = []
+	for vector in eigen_info[1]:
+		eigenvectors.append(np.dot(new_mat, vector))
+	eigenmap = dict()
+	for i in range(len(eigen_info[0])):
+		if eigen_info[0][i] in eigenmap:
+			eigenmap[eigen_info[0][i]].append(eigen_info[1][i])
+		else:
+			vectors = list()
+			vectors.append(eigen_info[1][i])
+			eigenmap[eigen_info[0][i]] = vectors
+	sorted_eigenvals = sorted(eigenmap.keys())
+	print sorted_eigenvals
+	return (eigen_info[0], eigenvectors)
 
 def mean_column(matrix):
 	return np.mean(matrix, axis=1)
@@ -43,19 +55,19 @@ def mean_subtract(matrix):
 
 def covariance_matrix(matrix):
 	mat = mean_subtract(matrix)
-	print "covariance matrix shape"
-	print np.shape(matrix)
 	return np.dot(mat, mat.transpose())/np.shape(matrix)[0]
 
+def eigenvalues(matrix):
+	eigen_info = np.linalg.eig(matrix)
+	return eigen_info
+
 def get_eigenvalues(matrix):
-	print "printing mat shape"
-	print np.shape(matrix)
-	print "print mat"
 	mat =  np.dot(matrix.transpose(), matrix)
-	print np.shape(mat)
-	print matrix
 	eigen_info = np.linalg.eig(np.dot(matrix.transpose(),matrix))
 	return eigen_info
+
+def a_transpose_a(matrix):
+	return np.dot(matrix.transpose(),matrix)
 
 def main():
 	#10000 28 x 28 test images
@@ -67,16 +79,17 @@ def main():
 	# 60000 labels
 	trainLabels = load("trainLabels.bin", (1, 60000))
 
-	hw1FindEigendigits(trainImages)
+	images = sample_images(trainImages, 5)
+	images = vectorize_images(images)
+	#cov = covariance_matrix(images)
+	#NOTE: Maybe subtract mean before getting eigenvalues
+	eigen_info = hw1FindEigendigits(images)
+	print eigen_info[0]
+	for i in range(len(eigen_info[0]) - 1):
+		print eigen_info[0][i] >= eigen_info[0][i + 1]
+	#hw1FindEigendigits(trainImages)
 	#plt.imshow(trainImages[:, :, 1])
 	#plt.show()
-
-	images = sample_images(testImages, 3)
-	images = vectorize_images(images)
-	cov = covariance_matrix(images)
-	eigen_info = get_eigenvalues(images)
-	print eigen_info
-
 
 if __name__ == '__main__':
 	main()
