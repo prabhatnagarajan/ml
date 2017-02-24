67c3c0de5215a85f6a35de61c8b09ed560f51cff
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2017-02-23 19:03:53 -0600

    load data

--- ica/ica.py
+++ ica/ica.py
@@ -1,5 +1,12 @@
+from load import load
+import numpy as np
+import matplotlib.pyplot as plt
+from pdb import set_trace
+import scipy.io
+
 def main():
-	print "Hello World"
+	sounds = load("data/sounds.mat", "sounds")
+	print np.shape(sounds)
 
 if __name__ == '__main__':
 	main()
