17ae275790bcc06e1dc69a201844e4468ebc444e
Prabhat <prabhat.nagarajan@gmail.com>
2017-05-07 18:58:13 -0500

    adds gp regressor

--- gp/gp.py
+++ gp/gp.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from extract import *
 import math
 from pdb import set_trace
-
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 
 filename = "data_GP/AG/block1-UNWEIGHTED-SLOW-NONDOMINANT-RANDOM/20161213203046-59968-right-speed_0.500.csv"
 time, data = extract_data(filename)
@@ -15,20 +16,31 @@ def main():
 		regression.append(i+1)
 
 	params = gradient_ascent(time[0::10], data[0::10],[0.1, 0.1, 0.03], np.array([0.005, 0.005, 0.0001]))
+	mean, covariance = gpr(time[0::10], data[0::10], time[1::2], params)
 
+	kernel = 0.01 * RBF(length_scale = 0.05, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(noise_level = 0.0001, noise_level_bounds=(1e-10, 1e+1))
+	gp = GaussianProcessRegressor(kernel=kernel, alpha = 0.0).fit(time[0::10].reshape(-1,1), data[0::10].reshape(-1, 1))
+	mean, covariance = gp.predict(np.array(time[1::2]).reshape(-1, 1), return_cov = True)
+	mean = np.squeeze(mean)
+	bottom = mean -  np.sqrt(np.diag(covariance))
+	top = mean +  np.sqrt(np.diag(covariance))
+	plt.plot(time, data, color="red")
+	plt.plot(time[1::2], mean, 'k', lw=3, zorder=9)
+	plt.fill_between(time[1::2], bottom, top, alpha=0.5, color='k')
+	plt.title("Gaussian Process")
+	plt.tight_layout()
 
-	plt.plot(time, data)
-	# plt.plot(data + 1)
 	plt.show()
+
 
 #Gets mean and covariance, similar to Marsland's algo in his GP Chapter
 def gpr(time, data, test_times, params):
 	K = kernel(time, params[0], params[1], params[2])[0]
 	kstar = kernel2(time, test_times, params[0], params[1], params[2])
 	kstarstar = kernel2(test_times, test_times, params[0], params[1], params[2])
-	mean = np.tranpose(kstar).dot(np.inv(K)).dot(data)
-	set_trace()
-	covariance = kstarstar - np.tranpose(kstar).dot(np.inv(K)).dot(kstar)
+	mean = np.transpose(kstar).dot(np.linalg.inv(K)).dot(data)
+	covariance = kstarstar - np.transpose(kstar).dot(np.linalg.inv(K)).dot(kstar)
+	return (mean, covariance)
 
 def k(x1, x2, sigma_f, l):
 	return math.pow(sigma_f, 2) * np.exp((-1) * (1/(2 * l * l)) * math.pow(x1 - x2, 2))
@@ -36,7 +48,7 @@ def k(x1, x2, sigma_f, l):
 def gradient_ascent(time, training_data, bounds, alphas):
 	sigma_f, sigma_l, sigma_n = get_random_parameters(bounds[0], bounds[1], bounds[2])
 	params = np.array([sigma_f, sigma_l, sigma_n])
-	for i in range(5000):
+	for i in range(500):
 		gradient = compute_derivatives(time, training_data, params)
 		params = params + alphas *  gradient
 		if i % 1000 == 0:
